9f4fbb600c62996ab6fbb09d35eafb0990f6ce02
Michael <michaelpoland2001@gmail.com>
2021-10-20 09:33:03 +0700

    first commit

---
+++ resources/views/create.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'Friends')
+
+@section('content')
+
+<form action="/friends" method='POST'>
+<div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Nama</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" nama="nama" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">No Telpon</label>
+    <input type="text" class="form-control" nama ="no_tlp"|id="exampleInputPassword1">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Alamat</label>
+    <input type="text" class="form-control" nama="alamat"|id="exampleInputPassword1">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+@endsection
